e1df1fac9f7f9d5f466ecf9c5300909dfe975ecb
Alchemy <Alchemy@192.168.1.13>
2020-01-06 22:51:28 -0800

    boxing

---
+++ 07_01/src/comm/example/Boxing.java
@@ -0,0 +1,17 @@
+package comm.example;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+		int pInt = 10;
+		Integer wInt= pInt; // auto boxing
+		int p2=wInt; // auto unboxing
+		System.out.println(wInt);
+		System.out.println(pInt);
+		
+		
+		
+		
+		
+	}
+}
